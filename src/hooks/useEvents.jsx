912c9efa3ac093f945a8d9fcc5bd22f31d3f305b
moshiur111 <moshiur.code@gmail.com>
2025-12-17 17:04:54 +0600

    feat(events): implement core event management functions

--- src/hooks/useEvents.jsx
+++ src/hooks/useEvents.jsx
@@ -1,10 +1,58 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 const useEvents = () => {
   const [state, setState] = useState({});
 
+  const getEventsByClockId = (clockId) => {
+    return Object.keys(state).filter((item) => item.startsWith(clockId));
+  };
+
+  const getEvents = (isArray = false) => {
+    if (!isArray) return state;
+    return Object.values(state);
+  };
+
+  const addEvent = (event) => {
+    event.id = uuidv4();
+    const { id, clockId } = event;
+    setState((prev) => ({
+      ...prev,
+      [`${clockId}|${id}`]: event,
+    }));
+    return event;
+  };
+
+  const deleteEvent = (id) => {
+    const events = { ...state };
+    delete events[id];
+    setState(events);
+  };
+
+  const deleteEventByClockId = (clockId) => {
+    const events = Object.keys(state).filter(
+      (item) => !item.startsWith(clockId)
+    );
+    setState(events);
+  };
+
+  const updateEvent = (updatedEvent, id) => {
+    const events = { ...state };
+    events[id] = {
+      ...events[id],
+      ...updatedEvent,
+    };
+    setState(events);
+  };
+
   return {
     events: state,
+    getEventsByClockId,
+    getEvents,
+    addEvent,
+    deleteEvent,
+    deleteEventByClockId,
+    updateEvent,
   };
 };
 
